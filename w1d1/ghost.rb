e01263f9bc2a4522a05579496f8ce82e08e49eed
Walter Tan <waltertan12@gmail.com>
2015-08-24 21:07:36 -0700

    Complete Phase 1 and 2 of Ghost

--- w1d1/ghost.rb
+++ w1d1/ghost.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :current_player
+  attr_reader :current_player, :dictionary
 
   def initialize(player1, player2)
     @player1 = player1
@@ -7,25 +7,45 @@ class Game
     @current_player = @player1
     @fragment = ""
     @dictionary = import_dictionary('ghost-dictionary.txt')
+    @losses = {@player1 => 0, @player2 => 0}
   end
 
   def import_dictionary(filename)
     result = {}
     File.readlines(filename).each do |line|
-      result[line] = line
+      result[line.chomp] = line.chomp
     end
     result
   end
 
-  def play_round
-    puts "Woo start!"
-    take_turn(@current_player)
-    next_player!
+  def run
+    until @losses.values.include?(5)
+      play_round
+      @fragment = ""
+    end
+    puts "And you are the loser"
+    puts "The game is over now... :("
   end
 
-  # def current_player
-  #   @current_player
-  # end
+  def record(player)
+    "GHOST"[0...@losses[player]]
+  end
+
+  def play_round
+    puts "Woo start!"
+    until round_over?
+      take_turn(@current_player)
+      puts "The current fragment is now #{@fragment}"
+      next_player!
+    end
+    @losses[previous_player] += 1
+    puts "You suck, #{previous_player.name}."
+    puts "You are now a #{record(previous_player)}"
+  end
+
+  def round_over?
+    !@dictionary[@fragment].nil?
+  end
 
   def previous_player
     if @current_player == @player1
@@ -40,10 +60,9 @@ class Game
   end
 
   def take_turn(player)
-    puts "Input guess, #{player.name}!"
+    puts "Input letter, #{player.name}"
     guess = player.guess
-    until valid_play?(@fragment + guess)
-      puts "Not valid guess: #{@fragment + guess}"
+    until valid_play?(guess)
       player.alert_invalid_guess
       guess = player.guess
     end
@@ -51,14 +70,14 @@ class Game
   end
 
   def valid_play?(string)
-    # puts "#{dictionary}"
+    return false if string.match(/^[a-zA-Z]$/).nil?
+    potential_fragment = @fragment + string
     @dictionary.each_key do |key|
-      puts "Key: #{key[0..string.length]}, String: #{string}"
-      return true if key[0...string.length] == string
+      return true if key[0...potential_fragment.length] == potential_fragment
     end
     false
-    # !@dictionary[string].nil?
   end
+
 end
 
 
