b39c040e5c35679f357c1ae333827a50eb5d540d
Walter Tan <waltertan12@gmail.com>
2015-08-29 12:05:40 -0700

    Allow Tile to store a board -- Lets the Tile find its neighbors a little easier

--- w1/w1d5/Tile.rb
+++ w1/w1d5/Tile.rb
@@ -1,13 +1,11 @@
 class Tile
-  attr_reader   :neighbors
-
-  def initialize(bomb)
-    @value     = "-"    # "-" indicates an unexplored cell
+  def initialize(bomb, board, pos)
     @bomb      = bomb   # true if Tile contains a bomb, false, otherwise
+    @board     = board
+    @position  = pos
+    @value     = "-"    # "-" indicates an unexplored cell
     @revealed  = false
     @flagged   = false
-    @neighbors = []     # Store neighboring Tiles into an Array
-    # @position  = nil
   end
 
   def reveal
@@ -29,8 +27,29 @@ class Tile
     self.value   = "-"
   end
 
+  def neighbors
+    if neighbors.empty?
+      neighboring_tiles = []
+
+      x, y = pos.first, pos.last
+
+      DELTAS.each do |delta|
+        dx, dy = delta.first, delta.last
+        neighboring_tiles << board[[x + dx, y + dy]] if valid?(x + dx, y + dy)
+      end
+
+      @neighbors = neighboring_tiles
+    else
+      @neighbors
+    end
+  end
+
+  def valid?(x, y)
+    x >= 0 && x < 9 && y >= 0 && y < 9
+  end
+
   def add_neighbor(neighbor)
-    neighbors << neighbor
+    neighbors << neighbor unless neighbors.include?(neighbor)
   end
 
   def neighbor_bomb_count
@@ -58,10 +77,21 @@ class Tile
   end
 
   def inspect
-    # "<Tile##{self.object_id}>, Position: #{position}, Value: '#{value}'"
-    "<Tile##{self.object_id}>, Value: '#{value}'"
+    "<Tile##{self.object_id}>, Position: #{position}, Value: '#{value}'"
+    # "<Tile##{self.object_id}>, Value: '#{value}'"
   end
 
   private
-    attr_accessor :revealed, :flagged, :value#, :position
+    attr_accessor :revealed, :flagged, :value, :position
+
+    DELTAS = [
+      [ 0, -1],
+      [ 0,  1],
+      [ 1, -1],
+      [ 1,  0],
+      [ 1,  1],
+      [-1, -1],
+      [-1,  0],
+      [-1,  1],
+    ]
 end
